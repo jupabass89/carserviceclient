71700e595be67f0c6040d681cba5724b4e943333
juangome <jupabass89@gmail.com>
2020-10-12 17:59:10 -0500

    adding the relation between car and owner at the car-list component

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CarService } from '../shared/car/car.service';
 import { GiphyService } from '../shared/giphy/giphy.service';
 
@@ -9,8 +10,12 @@ import { GiphyService } from '../shared/giphy/giphy.service';
 })
 export class CarListComponent implements OnInit {
   cars: Array<any>;
+  owners;
 
-  constructor(private carService: CarService, private giphyService: GiphyService) { }
+  constructor(private carService: CarService,
+    private giphyService: GiphyService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
     this.carService.getAll().subscribe(data => {
@@ -19,5 +24,33 @@ export class CarListComponent implements OnInit {
         this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
       }
     });
+    this.carService.getAllOwners().subscribe(data => {
+      this.owners = data;
+    });
+  }
+
+  getOwnersName(ownerDni): string {
+    try {
+      return this.owners._embedded.owners.find(item => item.dni === ownerDni).name;
+    } catch (e) {
+      return 'No owner';
+    }
+  }
+
+  getOwnersId(ownerDni) {
+    try {
+      return this.owners._embedded.owners.find(item => item.dni === ownerDni)._links.owner.href
+        .replace('http://thawing-chamber-47973.herokuapp.com/owners/', '');
+    } catch (e) {
+      return '';
+    }
+  }
+
+  redirectTo(car) {
+    if (this.getOwnersName(car.ownerDni) !== 'No owner') {
+      this.router.navigate(['owner-edit/' + this.getOwnersId(car.ownerDni)]);
+    }
   }
 }
+
+
